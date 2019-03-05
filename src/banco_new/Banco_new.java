f5b322e6cfb2928dc9a1664d4d1c17345808f50a
cristiane <cris_csr@yahoo.com.br>
2019-03-05 07:29:15 -0300

    first commit

---
+++ src/banco_new/Banco_new.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco_new;
+
+/**
+ *
+ * @author Cristiane
+ */
+public class Banco_new {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("banco new");
+    }
+    
+}
---
+++ src/banco_new/Cliente.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco_new;
+
+/**
+ *
+ * @author Cristiane
+ */
+public class Cliente {
+    String nome;
+    int ag;
+    Conta cc;
+
+     public String getNome() {
+        return this.nome;
+    }
+
+     public void setAg() {
+        System.out.println("Agencia");
+    }
+    
+}
